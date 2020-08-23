4c9a08ec4cc0d1d7989b327a8a77f30343b46fd4
heyzqt <heyzqt@qq.com>
2020-08-23 16:27:34 +0800

    自动校验表单数据validator

---
+++ src/components/ReceiverAccount.vue
@@ -0,0 +1,50 @@
+<template>
+  <a-input-group compact>
+    <a-select v-model="type" @change="handleSelectChange">
+      <a-select-option value="alipay">
+        支付宝账号
+      </a-select-option>
+      <a-select-option value="bank">
+        银行账户
+      </a-select-option>
+    </a-select>
+    <a-input
+      v-model="number"
+      style="width: 50%"
+      default-value=""
+      @change="handleInputChange"
+    />
+  </a-input-group>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Object
+    }
+  },
+  data() {
+    const { type, number } = this.value || {};
+    return {
+      type: type || "alipay",
+      number: number || ""
+    };
+  },
+  watch: {
+    value(val) {
+      Object.assign(this, val);
+    }
+  },
+  methods: {
+    handleSelectChange(val) {
+      this.$emit("change", { ...this.value, type: val });
+    },
+    handleInputChange(e) {
+      this.$emit("change", { ...this.value, number: e.target.value });
+    }
+  }
+};
+</script>
+
+<style></style>
