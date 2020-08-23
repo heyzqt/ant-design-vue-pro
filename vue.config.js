3f8e60f42d5039856d3435b0faadb442225132c5
heyzqt <heyzqt@qq.com>
2020-08-23 18:21:33 +0800

    定制主题,动态修改主题

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,38 @@
+const path = require("path");
+const AntDesignThemePlugin = require("antd-theme-webpack-plugin");
+
+const options = {
+  antDir: path.join(__dirname, "./node_modules/ant-design-vue"),
+  stylesDir: path.join(__dirname, "./src"),
+  varFile: path.join(
+    __dirname,
+    "./node_modules/ant-design-vue/lib/style/themes/default.less"
+  ),
+  themeVariables: ["@primary-color"],
+  generateOnce: false
+  // lessUrl: "https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.2/less.min.js",
+  // publicPath: "",
+  // customColorRegexArray: [] // An array of regex codes to match your custom color variable values so that code can identify that it's a valid color. Make sure your regex does not adds false positives.
+};
+
+const themePlugin = new AntDesignThemePlugin(options);
+
 module.exports = {
   lintOnSave: false,
   css: {
     loaderOptions: {
       less: {
+        modifyVars: {
+          "primary-color": "#1DA57A",
+          "link-color": "#1DA57A",
+          "border-radius-base": "2px" //定制主题
+        },
         javascriptEnabled: true
       }
     }
+  },
+  configureWebpack: {
+    plugins: [themePlugin]
   },
   chainWebpack: config => {
     const svgRule = config.module.rule("svg");
